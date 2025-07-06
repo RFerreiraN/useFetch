e41119c9b0237b530675a047e6cc6820a6eef71b
Ricardo Ferreira <ricardoferreiran@gmail.com>
2025-07-06 23:18:30 +0200

    cambios

--- src/Components/UsersComponent.jsx
+++ src/Components/UsersComponent.jsx
@@ -9,34 +9,32 @@ export const UsersComponent = () => {
     <>
       <h1>Lista de Usuarios</h1>
       <hr />
-      {isLoading 
-      ? <h4>Cargando...</h4>
-      : errors 
-      ? <p>Hubo errores { errors }</p>
-      : <table className="table table-dark">
-        <thead>
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Email</th>
-            <th scope="col">Website</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(data) && data.map(user => (
-            <tr key={user.id}>
-              <th scope="row">{user.id}</th>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.website}</td>
-            </tr>
-          ))}
+      {isLoading
+        ? <h4>Cargando...</h4>
+        : errors
+          ? <p>Hubo errores {errors}</p>
+          : <table className="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              {Array.isArray(data) && data.map(user => (
+                <tr key={user.id}>
+                  <th scope="row">{user.id}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.website}</td>
+                </tr>
+              ))}
 
-        </tbody>
-      </table>
-    }
-      
+            </tbody>
+          </table>
+      }
     </>
-
   )
 }
